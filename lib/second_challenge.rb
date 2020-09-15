1ee6a7200d9f6cf0462ec28045e59650b71dda05
Moose Davis <moosedavis2011@gmail.com>
2020-09-15 17:42:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+      :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
     }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroin => {},
-      :heroine_friends => []
+      :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroin => {},
+        :heroine_friends => []
       
     }
 }
